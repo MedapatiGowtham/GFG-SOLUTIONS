62ce4bab0e4f47b81076a1cb94c66c13e2dcf61c
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-07 23:32:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Most Frequent Word/second-most-frequent-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int secFrequent(String[] arr) {
+        if (arr.length == 0) {
+            return -1;
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String s : arr) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        int first = 0;
+        int second = 0;
+
+        for (int freq : map.values()) {
+            if (freq > first) {
+                second = first;
+                first = freq;
+            } else if (freq > second && freq < first) {
+                second = freq;
+            }
+        }
+
+        return second == 0 ? -1 : second;
+    }
+}
