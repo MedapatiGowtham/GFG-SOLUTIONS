c31fa090fecb593baef097e3f8681e014be9ef9d
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-22 22:30:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Pairs in a sorted array whose sum is less than target/count-pairs-in-a-sorted-array-whose-sum-is-less-than-target.java
@@ -0,0 +1,17 @@
+class Solution {
+    int countPairs(int arr[], int target) {
+        int i = 0;
+        int j = arr.length - 1;
+        int count = 0;
+
+        while (i < j) {
+            if (arr[i] + arr[j] < target) {
+                count += j - i;
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return count;
+    }
+}
